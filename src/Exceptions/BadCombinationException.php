859bb757963e4cee5cdf08dd85554c29716d26f6
John Rayes <live627@gmail.com>
2016-07-16 19:57:04 -0700

    forgot these

---
+++ src/Exceptions/BadCombinationException.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @package AddonHelper
+ * @version 1.0
+ * @author John Rayes <live627@gmail.com>
+ * @copyright Copyright (c) 2011-2016, John Rayes
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+namespace live627\AddonHelper\Exceptions;
+
+class BadCombinationException extends ValidationException {}
+
