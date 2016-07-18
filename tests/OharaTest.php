31e916e07b89cf010ac7ab6d51c45514f9bce9b3
John Rayes <live627@gmail.com>
2016-07-17 23:09:39 -0700

    Update OharaTest.php

--- tests/OharaTest.php
+++ tests/OharaTest.php
@@ -1,6 +1,7 @@
 <?php
 
-require_once(__DIR__ . '/../vendor/elkarte/elkarte/tests/travis-ci/bootstrap.php');
+//~ require_once(__DIR__ . '/../vendor/elkarte/elkarte/tests/travis-ci/bootstrap.php');
+require_once(__DIR__ . '/bootstrap.php');
 
 $txt['months_title'] = 'Months';
 $txt['MockOhara_months'] = array(1 => 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
