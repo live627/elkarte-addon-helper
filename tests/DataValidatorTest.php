c0052e354615e927e8ee6bd8af293fd685eb5b77
John Rayes <live627@gmail.com>
2016-10-15 23:58:15 -0700

    use the container whenever possible

--- tests/DataValidatorTest.php
+++ tests/DataValidatorTest.php
@@ -10,7 +10,8 @@ class DataValidatorTest extends \PHPUnit_Framework_TestCase
 
     public function setUp()
     {
-        $this->validator = new DataValidator;
+        $obj=new MockOhara;
+        $this->validator = $obj->getContainer()->get('datavalidator');
     }
 
     /**
