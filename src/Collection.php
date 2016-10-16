b817fcbdb025dc51183b81194b982ee892fa4d3b
John Rayes <live627@gmail.com>
2016-10-16 00:12:56 -0700

    add basic collection

---
+++ src/Collection.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * @package AddonHelper
+ * @version 1.0
+ * @author John Rayes <live627@gmail.com>
+ * @copyright Copyright (c) 2011-2016, John Rayes
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+
+namespace live627\AddonHelper;
+
+class Collection implements \IteratorAggregate
+{
+    /**
+     * The array that holds all the items collected by the object.
+     *
+     * @var array
+     * @access private
+     */
+    private $items = array();
+
+    /**
+     * Appends a value to the object
+     *
+     * This method is chainable.
+     *
+     * @param mixed  The value to set
+     * @access protected
+     * @return Collection
+     */
+    protected function add($item)
+    {
+        $this->items[] = $item;
+
+        return $this;
+    }
+
+    /**
+     * Retrieve an external iterator.
+     *
+     * @access public
+     * @abstracting IteratorAggregate
+     * @return Generator
+     */
+    public function getIterator()
+    {
+        foreach ($this->items as $item) {
+            yield $item;
+        }
+    }
+}
